28dfc9a263ac522a47ecd20bd5f9d40e01f4c729
Isaac Hartung <isaac.hartung@gmail.com>
2021-08-06 15:00:06 -0400

    adding api, will update after frontend update

--- counterpoint/counterpoint/urls.py
+++ counterpoint/counterpoint/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import counterpoint
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('counterpoint/', counterpoint),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from musicTools.contrapunctus import Contrapunctus
+
+def counterpoint(request):
+    result = Contrapunctus(request.POST['key'])
+    return result.generate(request.POST['melody'], request.POST['vertical'])
+
 
 # Create your views here.
