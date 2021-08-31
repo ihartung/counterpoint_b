2067181cafd26c82c152b0b16d3eccf50a69ecf0
Isaac Hartung <isaac.hartung@gmail.com>
2021-08-31 10:35:18 -0400

    adding ping and csrf apis

--- counterpoint/urls.py
+++ counterpoint/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from main.views import counterpoint
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('counterpoint', counterpoint),
+    path('csrf', views.csrf),
+    path('ping', views.ping),
 ]
