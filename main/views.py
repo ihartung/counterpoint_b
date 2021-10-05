07138cbf46c2c91138f9afda5f02cd38a272b004
Isaac Hartung <isaac.hartung@gmail.com>
2021-10-04 22:16:26 -0400

    limit melodies to 50 notes

--- main/views.py
+++ main/views.py
@@ -6,6 +6,8 @@ from django.http import JsonResponse
 def counterpoint(request):
     result = Contrapunctus(request.POST['key'])
     melody = request.POST['melody'].split(',')
+    if len(melody) > 50:
+        return JsonResponse({'message':'melody too long, must be 50 notes or less'})
     melody = list(map(int, melody))
     return JsonResponse({'counterpoint':result.generate(melody, int(request.POST['vertical']))})
 
